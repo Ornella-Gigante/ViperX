0f0bbce10a6774f5a348865e4feeeaa6b52b4fa2
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-07-27 19:48:51 +0200

    feat: add Direction enum with opposite direction logic

--- app/src/main/java/es/nellagames/viperx/Direction.java
+++ app/src/main/java/es/nellagames/viperx/Direction.java
@@ -1,4 +1,11 @@
 package es.nellagames.viperx;
 
-public class Direction {
+
+public enum Direction {
+    UP, DOWN, LEFT, RIGHT;
+
+    public boolean isOpposite(Direction d) {
+        return (this == UP && d == DOWN) || (this == DOWN && d == UP)
+                || (this == LEFT && d == RIGHT) || (this == RIGHT && d == LEFT);
+    }
 }
